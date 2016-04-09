b64653a22d566966441117c6f6ad606751f10969
Philipp Küng <git@philippkueng.ch>
2016-04-09 12:54:08 +0200

    replace the dummy data with the shareholders and shareholder relations

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-  
+'use strict';
+
 var units = "Widgets";
 
 var margin = {top: 10, right: 10, bottom: 10, left: 10},
@@ -25,101 +26,142 @@ var sankey = d3.sankey()
 
 var path = sankey.link();
 
-// load the data (using the timelyportfolio csv method)
-d3.csv("sankey.csv", function(error, data) {
+// load the shareholders
+d3.csv("shareholder.csv", function (error, shareholders) {
 
-  //set up graph in same style as original example but empty
-  graph = {"nodes" : [], "links" : []};
+  // load the data (using the timelyportfolio csv method)
+  d3.csv("shareholder_relation.csv", function(error, shareholder_relation) {
 
-    data.forEach(function (d) {
-      graph.nodes.push({ "name": d.source });
-      graph.nodes.push({ "name": d.target });
-      graph.links.push({ "source": d.source,
-                         "target": d.target,
-                         "value": +d.value });
-     });
+    var findShareholderById = function (shareholder_id) {
+      return _.filter(shareholder_relation, function (relation) {
+        return relation.owner === shareholder_id;
+      });
+    };
 
-     // return only the distinct / unique nodes
-     graph.nodes = d3.keys(d3.nest()
-       .key(function (d) { return d.name; })
-       .map(graph.nodes));
+    var findNameById = function (shareholder_id) {
+      return _.find(shareholders, function (shareholder) {
+        return shareholder.id === shareholder_id;
+      })
+    };
 
-     // loop through each link replacing the text with its index from node
-     graph.links.forEach(function (d, i) {
-       graph.links[i].source = graph.nodes.indexOf(graph.links[i].source);
-       graph.links[i].target = graph.nodes.indexOf(graph.links[i].target);
-     });
+    console.log(findShareholderById("3"));
+    console.log(findNameById("4"));
+ 
+    var pairs = _.flatten(_.map(shareholders, function (shareholder) {
+      var owners = findShareholderById(shareholder.id);
 
-     //now loop through each nodes to make nodes an array of objects
-     // rather than an array of strings
-     graph.nodes.forEach(function (d, i) {
-       graph.nodes[i] = { "name": d };
-     });
+      var response = _.map(owners, function (owner) {
+        var owner_details = findNameById(owner.owned);
+        var a =
+          {
+            source: shareholder.name,
+            target: owner_details.name,
+            form: shareholder.form,
+            share: owner.share
+          };
+        return a;
+      });
 
-  sankey
-    .nodes(graph.nodes)
-    .links(graph.links)
-    .layout(32);
+      return response;
+    }), false);
 
-// add in the links
-  var link = svg.append("g").selectAll(".link")
-      .data(graph.links)
-    .enter().append("path")
-      .attr("class", "link")
-      .attr("d", path)
-      .style("stroke-width", function(d) { return Math.max(1, d.dy); })
-      .sort(function(a, b) { return b.dy - a.dy; });
+    console.log(pairs);
 
-// add the link titles
-  link.append("title")
-        .text(function(d) {
-        return d.source.name + " → " + 
-                d.target.name + "\n" + format(d.value); });
+    //set up graph in same style as original example but empty
+    var graph = {"nodes" : [], "links" : []};
 
-// add in the nodes
-  var node = svg.append("g").selectAll(".node")
-      .data(graph.nodes)
-    .enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function(d) { 
-      return "translate(" + d.x + "," + d.y + ")"; })
-    .call(d3.behavior.drag()
-      .origin(function(d) { return d; })
-      .on("dragstart", function() { 
-      this.parentNode.appendChild(this); })
-      .on("drag", dragmove));
+      pairs.forEach(function (d) {
+        graph.nodes.push({ "name": d.source });
+        graph.nodes.push({ "name": d.target });
+        graph.links.push({ "source": d.source,
+                           "target": d.target,
+                           "value": +d.share });
+       });
 
-// add the rectangles for the nodes
-  node.append("rect")
-      .attr("height", function(d) { return d.dy; })
-      .attr("width", sankey.nodeWidth())
-      .style("fill", function(d) { 
-      return d.color = color(d.name.replace(/ .*/, "")); })
-      .style("stroke", function(d) { 
-      return d3.rgb(d.color).darker(2); })
-    .append("title")
-      .text(function(d) { 
-      return d.name + "\n" + format(d.value); });
+       // return only the distinct / unique nodes
+       graph.nodes = d3.keys(d3.nest()
+         .key(function (d) { return d.name; })
+         .map(graph.nodes));
 
-// add in the title for the nodes
-  node.append("text")
-      .attr("x", -6)
-      .attr("y", function(d) { return d.dy / 2; })
-      .attr("dy", ".35em")
-      .attr("text-anchor", "end")
-      .attr("transform", null)
-      .text(function(d) { return d.name; })
-    .filter(function(d) { return d.x < width / 2; })
-      .attr("x", 6 + sankey.nodeWidth())
-      .attr("text-anchor", "start");
+       // loop through each link replacing the text with its index from node
+       graph.links.forEach(function (d, i) {
+         graph.links[i].source = graph.nodes.indexOf(graph.links[i].source);
+         graph.links[i].target = graph.nodes.indexOf(graph.links[i].target);
+       });
 
-// the function for moving the nodes
-  function dragmove(d) {
-    d3.select(this).attr("transform", 
-        "translate(" + d.x + "," + (
-                d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))
-            ) + ")");
-    sankey.relayout();
-    link.attr("d", path);
-  }
+       //now loop through each nodes to make nodes an array of objects
+       // rather than an array of strings
+       graph.nodes.forEach(function (d, i) {
+         graph.nodes[i] = { "name": d };
+       });
+
+    sankey
+      .nodes(graph.nodes)
+      .links(graph.links)
+      .layout(32);
+
+  // add in the links
+    var link = svg.append("g").selectAll(".link")
+        .data(graph.links)
+      .enter().append("path")
+        .attr("class", "link")
+        .attr("d", path)
+        .style("stroke-width", function(d) { return Math.max(1, d.dy); })
+        .sort(function(a, b) { return b.dy - a.dy; });
+
+  // add the link titles
+    link.append("title")
+          .text(function(d) {
+          return d.source.name + " → " + 
+                  d.target.name + "\n" + format(d.value); });
+
+  // add in the nodes
+    var node = svg.append("g").selectAll(".node")
+        .data(graph.nodes)
+      .enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) { 
+        return "translate(" + d.x + "," + d.y + ")"; })
+      .call(d3.behavior.drag()
+        .origin(function(d) { return d; })
+        .on("dragstart", function() { 
+        this.parentNode.appendChild(this); })
+        .on("drag", dragmove));
+
+  // add the rectangles for the nodes
+    node.append("rect")
+        .attr("height", function(d) { return d.dy; })
+        .attr("width", sankey.nodeWidth())
+        .style("fill", function(d) { 
+        return d.color = color(d.name.replace(/ .*/, "")); })
+        .style("stroke", function(d) { 
+        return d3.rgb(d.color).darker(2); })
+      .append("title")
+        .text(function(d) { 
+        return d.name + "\n" + format(d.value); });
+
+  // add in the title for the nodes
+    node.append("text")
+        .attr("x", -6)
+        .attr("y", function(d) { return d.dy / 2; })
+        .attr("dy", ".35em")
+        .attr("text-anchor", "end")
+        .attr("transform", null)
+        .text(function(d) { return d.name; })
+      .filter(function(d) { return d.x < width / 2; })
+        .attr("x", 6 + sankey.nodeWidth())
+        .attr("text-anchor", "start");
+
+  // the function for moving the nodes
+    function dragmove(d) {
+      d3.select(this).attr("transform", 
+          "translate(" + d.x + "," + (
+                  d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))
+              ) + ")");
+      sankey.relayout();
+      link.attr("d", path);
+    }
+  });
+
 });
+
