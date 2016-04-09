fc9f88243f403a852659e6849bc54ebf8a95710e
Philipp Küng <git@philippkueng.ch>
2016-04-09 11:20:43 +0200

    adding a gulpfile with the build configuration

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    livereload = require('gulp-livereload');
+
+gulp.task('sass', function() {
+  gulp.src('./sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('css'))
+    .pipe(livereload());
+});
+
+gulp.task('html', function () {
+  gulp.src('./index.html')
+    .pipe(livereload());
+});
+
+gulp.task('javascript', function () {
+  gulp.src('./*.js')
+    .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('./sass/*.scss', ['sass']);
+  gulp.watch('./index.html', ['html']);
+  gulp.watch('./*.js', ['javascript']);
+  livereload();
+});
